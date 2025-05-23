3963366f10cd54533b7cfffc304cf746de02cbba
itsoctotv <octotv@protonmail.com>
2025-05-23 08:27:39 +0200

    update; convert binary to decimal

--- code/asm/helloworld.s
+++ code/asm/helloworld.s
@@ -7,6 +7,14 @@ E  = %10000000 		; Enable Signal
 RW = %01000000 		; Read Write Signal
 RS = %00100000 		; Register Select Signal
 
+; binary to decimal 
+value = $0200		; 2bytes
+mod10 = $0202       ; 2bytes
+message2 = $0204	; 6bytes
+
+
+
+
   .org $8000 		; origin of where the code goes in memory 
   
 reset:
@@ -20,7 +28,7 @@ reset:
   lda #%11111111 	; init interface adapter, set all PORTB pins to output
   sta DDRB
 
-  lda #%11100000 	; Set top three PORTA pins as output, used to control the LCD
+  lda #%11100001 	; Set top three PORTA pins as output, used to control the LCD
   sta DDRA
 
   
@@ -41,31 +49,100 @@ reset:
   lda #%00000001 	; clear display when reset
   jsr sendLCDInstruction
 
+  lda #0
+  sta message2
+
 ; END reset
 ; BEGIN
 ; -------
 
+  ; init value to be number to convert
+  lda number
+  sta value
+  lda number + 1
+  sta value + 1
+divide:
+  ; init remainder to be zero
+  lda #0
+  sta mod10
+  sta mod10 + 1
+  clc ; clear carry bit
 
 
+  ldx #16
+divloop:
+  ; rotate quotient of remainder
+  rol value
+  rol value + 1
+  rol mod10
+  rol mod10 + 1
+  ; A and Y = dividend - divisor
+  sec 
+  lda mod10
+  sbc #10
+  tay ; save low byte to Y register
+  lda mod10 + 1
+  sbc #0
+  bcc ignoreResult ; branch if dividend < divisor
+  sty mod10 
+  sta mod10 + 1
+  
+  
+ignoreResult:
+  dex
+  bne divloop
+  rol value ; shift in the last bit of the quotient
+  rol value + 1
+
+  lda mod10
+  clc
+  adc #"0"
+  jsr pushCharToString
+  ; if value != 0 -> continute dividing
+  lda value
+  ora value + 1
+  bne divide ; branch if value not zero
+
+  
+  ; print a string
   ldx #0 			; set the X register to 0, use it as a count
 					; at the end of the message .asciiz string there is a 0-byte,
 					; which will set the Zero flag when LDA is called -> branch-if-equal -> to the main loop (infinite)
 writeMessage:
-  lda message,x		; load char with x register as in msg + 1, msg + 2 etc
+  lda message2,x		; load char with x register as in msg + 1, msg + 2 etc
   beq loop			
   jsr printCharLCD  
   inx				; increment the x register
   jmp writeMessage
 
 
-
 loop:
+
   jmp loop
-; -----
-; END PROGRAM
+  
+number: .word 1738
+
+; Add the char in A registor to beginning of null-term string 'message2'
+pushCharToString:
+
+  pha ; push new first char onto stack
+  ldy #0 
+charLoop:
+  lda message2,y ; get char string, put into x
+  tax
+  pla 
+  sta message2,y ; pull char off stack and add it to the string
+  iny
+  txa
+  pha			 ; push char from string to stack
+  bne charLoop	 
+  pla 
+  sta message2,y ; pull the null off the stack and add to end of string
+
+  rts
 
 
-message: .asciiz "  Hello World!                           6502-Computer!"
+message: .asciiz " 6502-Computer! "
 
 
 
